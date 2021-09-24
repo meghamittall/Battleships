866143c8b31cbd360435e4758f8d1ce0223a87fa
Megha Mittal <mittal.megha927@gmail.com>
2021-09-24 14:19:19 +0530

    empty grid function

--- battleship.py
+++ battleship.py
@@ -62,7 +62,13 @@ Parameters: int ; int
 Returns: 2D list of ints
 '''
 def emptyGrid(rows, cols):
-    return
+    list=[]
+    for i in range(rows):
+        col = []
+        for j in range(cols):
+            col.append(EMPTY_UNCLICKED)
+        list.append(col)
+    return list
 
 
 '''
@@ -269,5 +275,6 @@ def runSimulation(w, h):
 # This code runs the test cases to check your work
 if __name__ == "__main__":
 
+    test.testEmptyGrid()
     ## Finally, run the simulation to test it manually ##
-    runSimulation(500, 500)
+    #runSimulation(500, 500)
